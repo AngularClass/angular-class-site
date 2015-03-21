e5fd9a32abc4a49adc30386bbef02169ad41dbdb
Hendrixer <willscottmoss@gmail.com>
2015-03-21 13:36:02 -0700

    chore(style): change from muti cards to one card

--- gulpfile.js
+++ gulpfile.js
@@ -191,8 +191,8 @@ gulp.task('pagespeed', pagespeed.bind(null, {
   // free (no API key) tier. You can use a Google
   // Developer API key if you have one. See
   // http://goo.gl/RkN0vE for info key: 'YOUR_API_KEY'
-  url: 'https://example.com',
-  strategy: 'mobile'
+  url: 'https://angularclass.com',
+  strategy: 'desktop'
 }));
 
 // Load custom tasks from the `tasks` directory
